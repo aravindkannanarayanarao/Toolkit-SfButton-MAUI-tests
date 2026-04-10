fdca7e65d24b22ed35d403e88657e2a02c776fc8
aravindkannanarayanarao <92799581+aravindkannanarayanarao@users.noreply.github.com>
2026-04-10 10:57:18 +0530

    uh

--- UITest/Appium/MAUIExpander/MAUIExpander/App.xaml.cs
+++ UITest/Appium/MAUIExpander/MAUIExpander/App.xaml.cs
@@ -7,6 +7,7 @@
         public static float Density { get; internal set; }
         public App()
         {
+			Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjGyl/VkV+XU9FfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hTH9RdEZiWn1ac3ZXR2FVWkd1");
             InitializeComponent();
 
             MainPage = new NavigationPage(new MainMenu());
